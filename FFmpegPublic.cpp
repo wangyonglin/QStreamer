4b02e387b7f32054d504259b2f1199d35352f11f
wangyonglin <admin@wangyonglin.com>
2024-01-16 20:33:51 +0800

    20240116

---
+++ FFmpegPublic.cpp
@@ -0,0 +1,6 @@
+#include "PublicFFmpeg.h"
+
+PublicFFmpeg::PublicFFmpeg()
+{
+
+}
---
+++ FFmpegPublic.h
@@ -0,0 +1,11 @@
+#ifndef PUBLICFFMPEG_H
+#define PUBLICFFMPEG_H
+
+
+class PublicFFmpeg
+{
+public:
+    PublicFFmpeg();
+};
+
+#endif // PUBLICFFMPEG_H
